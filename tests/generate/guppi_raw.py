f4074ad3025c154fcbe019bfa612ac0e951d2c55
radonnachie <code@radonn.co.za>
2022-05-25 18:00:33 +0000

    ^^ tests generation and run working...

--- tests/generate/guppi_raw.py
+++ tests/generate/guppi_raw.py
@@ -1,14 +1,23 @@
 from astropy import units as u
 import setigen as stg
 
-n_ant = 8
-n_chan_perant = 64
+n_pols = 2
+n_bits = 8
+n_ant = 64
+n_chan_perant = 128
+n_time = 1024
+
+
+file_bytesize = 1*(2**30)
+block_bytesize = (n_time*n_ant*n_chan_perant*n_pols*n_bits)//8
+
 n_obschan = n_ant*n_chan_perant
+blocks_per_file = file_bytesize // block_bytesize
 
 antenna = stg.voltage.Antenna(sample_rate=3e9*u.Hz,
                               fch1=6000e6*u.Hz,
                               ascending=True,
-                              num_pols=2)
+                              num_pols=n_pols)
 
 antenna.x.add_noise(v_mean=0,
                     v_std=1)
@@ -21,30 +30,30 @@ digitizer = stg.voltage.RealQuantizer(target_fwhm=32,
                                       num_bits=8)
 
 filterbank = stg.voltage.PolyphaseFilterbank(num_taps=8,
-                                             num_branches=1024)
+                                             num_branches=n_obschan*2)
 
 requantizer = stg.voltage.ComplexQuantizer(target_fwhm=32,
-                                           num_bits=8)
+                                           num_bits=n_bits)
 
-block_size = 128*(2**20)
-file_size = 16*(2**30)
-blocks_per_file = file_size // block_size
 rvb = stg.voltage.RawVoltageBackend(antenna,
                                     digitizer=digitizer,
                                     filterbank=filterbank,
                                     requantizer=requantizer,
                                     start_chan=0,
                                     num_chans=n_obschan,
-                                    block_size=block_size,
+                                    block_size=block_bytesize,
                                     blocks_per_file=blocks_per_file,
                                     num_subblocks=32)
 
 rvb.record(
-	output_file_stem='../golden_input',
-	num_blocks=blocks_per_file//16,
+	output_file_stem='../golden_synthesized_input',
+	num_blocks=(2**30)//block_bytesize,
 	length_mode='num_blocks',
 	header_dict={
-		'TELESCOP': 'SETIGEN'
+		'TELESCOP': 'SETIGEN',
+    'NANTS'   : str(n_ant),
+    'OBSID'   : 'SYNTHETIC',
+    'piperblk': n_time
 	},
 	verbose=True
 )
--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,5 @@
-echo ../init/readraw_init.sh cbf ${PWD} ./bf_recipe.h5 ./output golden_input.0000.raw
-../init/readraw_init.sh cbf ${PWD} ./bf_recipe.h5 ./output golden_input.0000.raw
+mkdir -p ./output
+rm ./output/*
+
+echo ../init/readraw_init.sh cbf ${PWD} ${PWD} ./output golden_synthesized_input.0000.raw
+../init/readraw_init.sh cbf ${PWD} ${PWD} ./output golden_synthesized_input.0000.raw
